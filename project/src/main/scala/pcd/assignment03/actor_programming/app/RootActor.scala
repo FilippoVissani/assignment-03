bf6fe2a6189fcd7ee406578c4b17531c7cc7616a
Filippo Vissani <filippo.vissani@studio.unibo.it>
2022-06-12 16:54:16 +0200

    Now actor system stops when simulation terminates

--- project/src/main/scala/pcd/assignment03/actor_programming/app/RootActor.scala
+++ project/src/main/scala/pcd/assignment03/actor_programming/app/RootActor.scala
@@ -1,9 +1,10 @@
 package pcd.assignment03.actor_programming.app
 
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Terminated}
 import pcd.assignment03.actor_programming.view.View
 import pcd.assignment03.actor_programming.view.actor.ViewActor
+import pcd.assignment03.actor_programming.controller.actor.SimulationControllerActor.ControllerActorCommand
 import pcd.assignment03.actor_programming.controller.actor.SimulationControllerActor.ControllerActorCommand.*
 import pcd.assignment03.actor_programming.controller.actor.{ChronometerActor, SimulationControllerActor}
 import pcd.assignment03.actor_programming.model.actor.BodyActor
@@ -22,7 +23,7 @@ object RootActor:
   export RootActorCommand.*
 
   def apply(bodiesNumber: Int, maxIterations: Long, withView: Boolean): Behavior[RootActorCommand] =
-    Behaviors.setup(ctx =>
+    Behaviors.setup[RootActorCommand](ctx =>
       var bodyActors: List[ActorRef[BodyActorCommand]] = List()
       val random: Random = Random(System.currentTimeMillis())
       val bounds: Boundary = Boundary(-6.0, -6.0, 6.0, 6.0)
@@ -31,22 +32,28 @@ object RootActor:
           ctx.log.info(s"Spawned Body actor id $i")
       val chronometerActor = ctx.spawn(ChronometerActor(), s"Chronometer-actor")
       ctx.log.info(s"Spawned Chronometer actor")
-      val controllerActor = ctx.spawn(SimulationControllerActor(bodyActors, maxIterations = maxIterations,chronometerActor = chronometerActor), s"Controller-actor")
+      val controllerActor: ActorRef[ControllerActorCommand] = ctx.spawn(SimulationControllerActor(bodyActors, maxIterations = maxIterations,chronometerActor = chronometerActor), s"Controller-actor")
       ctx.log.info(s"Spawned Controller actor")
       if withView then
         val viewActor = ctx.spawn(ViewActor(controllerActor, bounds, 800, 600), s"View-actor")
-          ctx.log.info(s"Spawned View actor")
+        ctx.log.info(s"Spawned View actor")
         controllerActor ! SetViewActor(viewActor)
+      else
+        ctx.watch(controllerActor)
 
-      Behaviors.receive((ctx, msg) => msg match
-      case Start => {
-        ctx.log.debug("Received StartSimulation")
-        controllerActor ! StartSimulation
-        Behaviors.same
-      }
-      case _ => {
-        ctx.log.debug("Received Stop")
-        controllerActor ! StopSimulation
+      Behaviors.receive[RootActorCommand]((ctx, msg) => msg match
+        case Start => {
+          ctx.log.debug("Received StartSimulation")
+          controllerActor ! StartSimulation
+          Behaviors.same
+        }
+        case _ => {
+          ctx.log.debug("Received Stop")
+          controllerActor ! StopSimulation
+          Behaviors.stopped
+        }
+      ).receiveSignal { case (ctx, Terminated(_)) =>
+        ctx.log.debug("Stopping actor system")
         Behaviors.stopped
       }
-    ))
+    )
--- project/src/main/scala/pcd/assignment03/actor_programming/app/SimulationAppGUI.scala
+++ project/src/main/scala/pcd/assignment03/actor_programming/app/SimulationAppGUI.scala
@@ -3,4 +3,4 @@ package pcd.assignment03.actor_programming.app
 import akka.actor.typed.ActorSystem
 
 object SimulationAppGUI extends App:
-  ActorSystem(RootActor(1000, 1000, true), "root")
+  ActorSystem(RootActor(100, 100, true), "root")
